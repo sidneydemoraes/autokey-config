c372a93a2d67c869caafc81779b1d206b6e70351
Sid <git@najjah.dev>
2024-03-28 17:09:58 -0300

    abbrevs removidas e adicionadas

--- data/Sample Scripts/Authy.py
+++
@@ -1,3 +0,0 @@
-# Enter script code
-
-system.exec_command("authy %U")
--- data/Sample Scripts/Startup.py
+++ data/Sample Scripts/Startup.py
@@ -1,6 +1,5 @@
 # Enter script code
 
-engine.run_script("Authy")
 engine.run_script("Vivaldi")
 import subprocess
 subprocess.run(["gnome-terminal"])
